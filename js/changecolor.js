3156b904d4d60563b4c7ea3d9482ccac899a9a33
CristianoLee <839658551@qq.com>
2022-03-04 22:54:53 +0800

    压缩了项目体积

--- js/changecolor.js
+++ js/changecolor.js
@@ -4,9 +4,9 @@ $('.change-color').on('click', () => {
   light = !light
   if (light) {
     $('.main-container').css({ backgroundColor: '#181c27' })
-    $('.head-bg').css({ background: 'url(./img/head.png)' })
+    $('.head-bg').css({ background: 'url(./image/head.png)' })
   } else {
     $('.main-container').css({ backgroundColor: '#f5f5f5' })
-    $('.head-bg').css({ background: 'url(./img/jie.png) 0 -310px' })
+    $('.head-bg').css({ background: 'url(./image/jie.png) 0 -310px' })
   }
 })
