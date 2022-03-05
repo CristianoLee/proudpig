7dca91e4b147bc60c5f87d24ae23a57d7c725931
CristianoLee <839658551@qq.com>
2022-03-05 22:35:38 +0800

    更新

--- js/changecolor.js
+++ js/changecolor.js
@@ -1,24 +1,29 @@
 // 切换肤色
-
 let color = localStorage.getItem('color')
 // 进入页面判断肤色
 if (color === 'light' || !color) {
-  $('.main-container').css({ backgroundColor: '#f5f5f5' })
-  $('.head-bg').css({ background: 'url(./image/head-light.png) 0 -310px' })
+  light()
 } else {
-  $('.main-container').css({ backgroundColor: '#181c27' })
-  $('.head-bg').css({ background: 'url(./image/head-dark.png) 0 -160px/1920px 1080px' })
+  dark()
 }
 
 $('.change-color').on('click', () => {
   if (color === 'light') {
     color = 'dark'
-    $('.main-container').css({ backgroundColor: '#181c27' })
-    $('.head-bg').css({ background: 'url(./image/head-dark.png) 0 -160px/1920px 1080px' })
+    dark()
   } else {
     color = 'light'
-    $('.main-container').css({ backgroundColor: '#f5f5f5' })
-    $('.head-bg').css({ background: 'url(./image/head-light.png) 0 -310px' })
+    light()
   }
   localStorage.setItem('color', color)
 })
+
+function light() {
+  $('.main-container').css({ backgroundColor: '#f5f5f5' })
+  $('.head-bg').css({ background: 'url(./image/head-light.png) 0 -310px' })
+}
+
+function dark() {
+  $('.main-container').css({ backgroundColor: '#181c27' })
+  $('.head-bg').css({ background: 'url(./image/head-dark.png) 0 -160px/1920px 1080px' })
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
-// 判断当前页面肤色
+window.onload = () => {
+  console.log(
+    '页面加载完毕！消耗了 %c' + Math.round(100 * performance.now()) / 100 + ' ms',
+    'background:#282c34;color:#51aded;'
+  )
+}
+
+// 初始化工具位置
+$('.backTop').removeClass('backTopUp').addClass('backTopDown')
+$('.tools').removeClass('toolsUp').addClass('toolsDown')
+
+// 记录上一次滚动高度，用于判断滚动方向
+let lastHeight = window.innerHeight
+
+// 仅处理鼠标滚动
+window.addEventListener('wheel', () => {
+  // console.log(window.scrollY);
+  if (window.scrollY > 300) {
+    $('.tools').removeClass('toolsDown').addClass('toolsUp')
+    $('.backTop').removeClass('backTopDown').addClass('backTopUp')
+  } else {
+    $('.tools').removeClass('toolsUp').addClass('toolsDown')
+    $('.backTop').removeClass('backTopUp').addClass('backTopDown')
+  }
+  if ((lastHeight < window.scrollY) & (window.scrollY > 300)) {
+    // 向下滚动
+    $('.t1').removeClass('scrollUp').addClass('scrollDown')
+  }
+  if (lastHeight > window.scrollY) {
+    // 向上滚动
+    $('.t1').removeClass('scrollDown').addClass('scrollUp')
+  }
+  // 相等则不做处理
+  lastHeight = window.scrollY
+})
 
 // 计算网站创建至今时长
 function buildTime(res) {
